5f1c083d6214333325ce70a4d5a915264f52f6f7
João Victor Camargo <jc7155004@gmail.com>
2022-07-02 15:40:04 -0300

    store

---
+++ src/state/reducers/repositoriesReducer.ts
@@ -0,0 +1,32 @@
+/* redux store
+repositories return
+ "data"<List of repositories from NPM>;
+ "loading"<True/False wheter we are fatching data>;
+ "error"<String, error message if one ocurred during fetch> */
+
+import { ActionType } from "../action-types";
+import { Action } from "../actions";
+
+interface RepositoriesState {
+  loading: boolean;
+  error: string | null;
+  data: string[];
+}
+
+const reducer = (
+  state: RepositoriesState,
+  action: Action
+): RepositoriesState => {
+  switch (action.type) {
+    case ActionType.SEARCH_REPOSITORIES:
+      return { loading: true, error: null, data: [] };
+    case ActionType.SEARCH_REPOSITORIES_SUCCESS:
+      return { loading: false, error: null, data: action.payload };
+    case ActionType.SEARCH_REPOSITORIES_ERROR:
+      return { loading: false, error: action.payload, data: [] };
+    default:
+      return state;
+  }
+};
+
+export default reducer;
