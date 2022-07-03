1c0f3993868c9ef4775f73da17ed354430c56a62
João Victor Camargo <jc7155004@gmail.com>
2022-07-03 10:08:02 -0300

    ReturnType

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import App from "./components/App";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <h1>oi</h1>
+    <App />
   </React.StrictMode>
 );
